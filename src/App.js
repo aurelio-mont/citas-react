f3a15ea1c7e2b5845b9fb837c97a79138174c0a2
aurelio-mont <aurelio.mont25@gmail.com>
2020-06-13 17:44:52 -0500

    Creando componente Cita

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
   // Arreglo de citas
@@ -22,7 +23,15 @@ function App() {
               crearCita={crearCita}
             />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita 
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
